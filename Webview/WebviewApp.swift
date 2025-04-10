b96dbee332b3c08f6f5179e246e6cf4981872da2
mkolendiuk-plana <mkolendiuk@planatechnologies.com>
2025-04-10 17:48:41 +0300

    Initial Commit

---
+++ Webview/WebviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebviewApp.swift
+//  Webview
+//
+//  Created by Maks Kolendiuk on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WebviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
